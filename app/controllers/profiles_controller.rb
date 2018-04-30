ed6bef43a1bb400ca0f6dbbcb5b926edc0ed0682
T Amos <taningreen@gmail.com>
2018-04-30 13:39:50 +0000

    Added helpful copy

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
@@ -13,13 +15,40 @@ def create
   @profile = @user.build_profile( profile_params )
   if @profile.save
     flash[:success] = "Profile updated!"
-    redirect_to user_path( params[:user_id] )
+    redirect_to user_path(id: params[:user_id] )
   else
     render action: :new
   end
 end
+
+#Get request to edit
+def edit
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+end  
+
+
+#Path to user update
+def update
+   @user = User.find( params[:user_id] )
+   @profile = @user.profile
+ if @profile.update_attributes(profile_params)
+    flash[:success] = "Profile updated!"
+    #redirect user to profile page
+    redirect_to user_path(id: params[:user_id] )
+ else 
+  render action: :edit
+ end  
+end 
+
+
 private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to(root_url) unless @user == current_user
   end
 end
